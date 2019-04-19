d3111ceaa369f3506a7fc4447ef02117b0381f96
galihiki <galhiki@gmail.com>
2019-04-19 12:06:39 +0300

    Updates

---
+++ precache-manifest.2e9151142b132266c6fd1b12456987dd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "038972589bc585d358a1",
+    "url": "/onlineStore/static/js/runtime~main.cca8e2b1.js"
+  },
+  {
+    "revision": "2b711c5d7c2d9837c17b",
+    "url": "/onlineStore/static/js/main.afb66f5c.chunk.js"
+  },
+  {
+    "revision": "80464e3c84a9c0e09988",
+    "url": "/onlineStore/static/js/2.c723df33.chunk.js"
+  },
+  {
+    "revision": "2b711c5d7c2d9837c17b",
+    "url": "/onlineStore/static/css/main.c89b832d.chunk.css"
+  },
+  {
+    "revision": "80464e3c84a9c0e09988",
+    "url": "/onlineStore/static/css/2.ede81d16.chunk.css"
+  },
+  {
+    "revision": "3d723255a4dd449a6707d9de29367b05",
+    "url": "/onlineStore/index.html"
+  }
+];
